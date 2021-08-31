43cf4737d09dff6de266bdc8bb3108fca9bae05b
grandonk666 <grandonk457@gmail.com>
2021-09-01 05:35:33 +0700

    improving UI

--- components/Categories/CategoriesItem.js
+++ components/Categories/CategoriesItem.js
@@ -7,7 +7,11 @@ const CategoriesItem = ({ category, currentSlide, slide }) => {
     <Link href={`/categories/${category.slug}`}>
       <a className="block w-full h-[30vh] md:h-[60vh] px-1 lg:px-3">
         <div className="relative w-full h-full shadow-md overflow-hidden group">
-          <div className="w-full h-full transition-all duration-500 transform group-hover:scale-125 bg-cover bg-center">
+          <div
+            className={`w-full h-full transition-all duration-500 bg-cover bg-center ${
+              currentSlide == slide ? "scale-125" : "scale-100"
+            }`}
+          >
             <Image
               src={getMediaURL(category.image.formats.medium)}
               alt={category.name}
@@ -19,9 +23,9 @@ const CategoriesItem = ({ category, currentSlide, slide }) => {
               loading="lazy"
             />
           </div>
-          <div className="absolute inset-0 bg-black bg-opacity-30 flex justify-center items-center overflow-hidden">
+          <div className="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-70 transition-all flex justify-center items-center overflow-hidden">
             <h2
-              className={`text-3xl md:text-6xl text-white text-center uppercase font-semibold transition-all duration-300 ${
+              className={`text-3xl md:text-6xl text-white text-center uppercase font-semibold transition-all duration-300 group-hover:scale-125 ${
                 currentSlide == slide ? "translate-y-0" : "translate-y-80"
               }`}
             >
