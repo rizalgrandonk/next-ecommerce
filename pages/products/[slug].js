4601882696ad032297c2de3be81c484e8622ff92
grandonk666 <grandonk457@gmail.com>
2021-09-09 19:54:31 +0700

    fixing data fetching

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -10,16 +10,20 @@ import { useCart } from "@/contexts/CartContext";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import Meta from "@/components/Meta";
 
-const Product = ({ product }) => {
+const Product = (props) => {
   const router = useRouter();
   const { locale } = router;
   const slug = router.query.slug;
 
   const { addItem, inCart } = useCart();
 
-  // const { data: product } = useSWR("product", () => getProducts(`/${slug}`), {
-  //   initialData: props.product,
-  // });
+  const { data: product } = useSWR(
+    `categories/${slug}`,
+    () => getProducts(`/${slug}`),
+    {
+      initialData: props.product,
+    }
+  );
 
   const keywords = product.categories.reduce((acc, prev) => {
     return `${acc}, ${prev.name.toLowerCase()}`;
